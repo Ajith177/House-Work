fee5383ae62dd79a5a2c93a19836aecd6222e3ec
Ajith177 <152384123+Ajith177@users.noreply.github.com>
2023-12-06 14:26:45 +0530

    Add files via upload

---
+++ src/Edit.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import "./First.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+
+function Edit({ setComplete, editData }) {
+//   const dating = useLocation;
+//   const getting_data = dating.state;
+const navigating=useNavigate()
+  const [data, setData] = useState({
+    title: editData.title,
+    body: editData.body,
+  });
+
+  const handleInput = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/posts`)
+      .then((response) => {
+        console.log(response.data);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  const clicking = () => {
+    navigating('/First')
+
+    const future = {
+      title: data.title,
+      body: data.body,
+    }
+    axios
+      .put(`http://localhost:3030/posts/${editData.id}`, future)
+      .then((response) => {
+        axios
+          .get(`http://localhost:3030/posts`)
+          .then((response) => {
+            setComplete(response.data);
+          })
+
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="window-box">
+      TITLE:
+      <input
+        type="text"
+        placeholder="TITLE"
+        className="text-field"
+        name="title"
+        value={data.title}
+        onChange={handleInput}
+      />
+      <br />
+      BODY:
+      <input
+        type="text"
+        placeholder="BODY"
+        className="text-field"
+        name="body"
+        value={data.body}
+        onChange={handleInput}
+      />
+      <br />
+      <button className="button-1" onClick={clicking}>
+        UPDATE
+      </button>
+    </div>
+  );
+}
+
+export default Edit;
---
+++ src/First.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './First.css';
+import { useNavigate } from 'react-router-dom';
+
+
+function First({ complete, setComplete, setEditData }) {
+  console.log(complete)
+  const [basket, setBasket] = useState({
+    title: "",
+    body: "",
+  })
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:3030/posts')
+      .then(response => {
+        setComplete(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  console.log("complete", complete)
+
+  const handleChange = (e) => {
+    setBasket({ ...basket, [e.target.name]: e.target.value })
+  }
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if (basket.length < 1) {
+      alert('The fields should not be empty')
+    }
+
+    else {
+      const future = {
+        title: basket.title,
+        body: basket.body,
+      };
+      axios.post('http://localhost:3030/posts', future)
+        .then((response) => {
+          axios.get('http://localhost:3030/posts')
+            .then(response => {
+              setComplete(response.data)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+
+  const editing = (item) => {
+    setEditData(item)
+    navigate(`/Edit`)
+  }
+
+  const deleting = (id, e) => {
+    axios.delete(`http://localhost:3030/posts/${id}`)
+      .then((res) => {
+        console.log(res)
+        alert(`The data with the ${id} was deleted`)
+      })
+      .catch((error) => {
+        console.log(error)
+        alert(`The data ${id} is not deleted`)
+      })
+  }
+  return (
+    <div>
+      {
+        complete.map((items) => {
+          return (
+            <div className='all' key={items.id}>
+              <p><b>id:</b>{items.id}</p>
+              <p><b>title:</b>{items.title}</p>
+              <p><b>body:</b>{items.body}</p>
+              <div className='subclass'>
+                <button className='button-1' onClick={e => editing(items)}>EDIT</button>
+                <button className='button-1' onClick={e => deleting(items.id, e)}>DELETE</button>
+              </div>
+            </div>
+          )
+        })
+      }
+      <div className='first-1'>
+        title:<input type="text" placeholder="title" onChange={handleChange} className='text-box' autoFocus name="title" /><br />
+        body:<textarea type="text" placeholder="body" onChange={handleChange} className='text-box' autoFocus name="body" /><br />
+        <button className='button-1' onClick={handlesubmit}>SUBMIT</button>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default First;
